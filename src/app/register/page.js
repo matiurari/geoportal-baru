e6c153000141ed780dcae2a5869116e2151432cd
matiurari <matiurari0@gmail.com>
2026-09-18 13:36:48 +0700

    Finishing project

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,6 +1,23 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import RegisterForm from './RegisterForm'
+import { palette } from "../../theme/theme"
+
 const page = () => {
     return (
-        <div>page</div>
+        <Box
+            sx={{
+                minHeight: "100vh",
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: palette.bg,
+                px: 2,
+            }}
+        >
+            <RegisterForm />
+        </Box>
     )
 }
 
